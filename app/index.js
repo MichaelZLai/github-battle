fef540d50b10af48397810fb89be1d48d9b971cc
Michael Lai <mike24567@gmail.com>
2016-12-18 18:59:32 -0500

    added avatar down

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 var USER_DATA = {
-  name: "Michael Lai",
+  name: "bunker Lai",
   username: "michaelzlai",
   image: "https://scontent.xx.fbcdn.net/v/t1.0-9/15181240_10154239531058790_7224650531886659620_n.jpg?oh=74b0673e2204b248f5646131230e447d&oe=58F90CDF"
 }
@@ -9,16 +9,46 @@ var ReactDOM = require("react-dom");
 
 //function(data) = view
 
-var HelloWorld = React.createClass({
-  render: function() {
+var ProfilePic = React.createClass({
+  render: function(){
+    return <img src={this.props.imageUrl} style={{height:100, width:100}} />
+  }
+})
+
+var ProfileLink = React.createClass({
+  render: function(){
     return(
-      <div> Hello, {this.props.name} </div>
+      <div>
+        <a href={"https://www.github.com/" + this.props.username} target="_blank">
+        {this.props.username}
+        </a>
+      </div>
+    )
+  }
+})
+
+var ProfileName = React.createClass({
+  render: function(){
+    return(
+      <div>{this.props.name}</div>
+    )
+  }
+})
+
+var Avatar = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
     )
   }
 })
 
 
 ReactDOM.render(
-  <HelloWorld name="MichaeL" />,
+  <Avatar user={USER_DATA} />,
   document.getElementById("app")
 )
